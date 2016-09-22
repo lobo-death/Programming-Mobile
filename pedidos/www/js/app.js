178c2dca2c116d571247f13a291a354cbaec8047
MARCELO MACHADO PEREIRA <mpereira@hbb.local>
2016-09-22 16:58:47 -0300

    First commit

---
+++ pedidos/www/js/app.js
@@ -0,0 +1,36 @@
+$('.collection')
+.on('click', '.collection-item', function(){
+		var $badge = $('.badge', this);
+		if ($badge.length == 0) {
+			$badge = $('<span class="badge brown-text">0</span>').appendTo(this);
+		}
+		$badge.text(parseInt($badge.text()) + 1);
+		var nomeProduto = this.firstChild.textContent;
+		Materialize.toast(nomeProduto + ' adicionado',1000);
+	}
+
+)
+
+$('#confirmar').on('click',function(){
+	var texto ="";
+	$('.badge').parent().each(function(){
+		texto += this.firstChild.textContent + ': ';
+		texto += this.lastChild.textContent + ', ';
+	});
+	$('#resumo').empty().text(texto);
+});
+
+$('.modal-trigger').leanModal();
+
+$('.collection')
+.on('click', '.badge', function(){
+	$(this).remove();
+		return false;
+	}
+);
+
+$('.acao-limpar').on('click', function() {
+	$('#numero-mesa').val('');
+	$('.badge').remove();
+	}
+);
